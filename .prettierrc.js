aec2d67049533e492ab5f8b794f7dcbfff851c1e
sanjorcool <498683134@qq.com>
2023-08-17 23:49:11 +0800

    feat: add commit lint

--- .prettierrc.js
+++ .prettierrc.js
@@ -8,17 +8,17 @@ module.exports = {
   // 行尾需要有分号
   semi: true,
   // 使用单引号
-  singleQuote: true,
+  singleQuote: false,
   // 对象的 key 仅在必要时用引号
-  quoteProps: 'as-needed',
+  quoteProps: "as-needed",
   // jsx 不使用单引号，而使用双引号
   jsxSingleQuote: false,
   // 末尾需要有逗号
-  trailingComma: 'all',
+  trailingComma: "all",
   // 大括号内的首尾需要空格
   bracketSpacing: true,
   // 箭头函数，只有一个参数的时候，也需要括号
-  arrowParens: 'always',
+  arrowParens: "always",
   // 每个文件格式化的范围是文件的全部内容
   rangeStart: 0,
   rangeEnd: Infinity,
@@ -27,15 +27,15 @@ module.exports = {
   // 不需要自动在文件开头插入 @prettier
   insertPragma: false,
   // 使用默认的折行标准
-  proseWrap: 'preserve',
+  proseWrap: "preserve",
   // 根据显示样式决定 html 要不要折行
-  htmlWhitespaceSensitivity: 'css',
+  htmlWhitespaceSensitivity: "css",
   // vue 文件中的 script 和 style 内不用缩进
   vueIndentScriptAndStyle: false,
   // 换行符使用 lf
-  endOfLine: 'lf',
+  endOfLine: "lf",
   // 格式化嵌入的内容
-  embeddedLanguageFormatting: 'auto',
+  embeddedLanguageFormatting: "auto",
   // html, vue, jsx 中每个属性占一行
   singleAttributePerLine: false,
 };
